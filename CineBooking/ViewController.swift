565dde420a3f2751b65412c0a26d421028ce4258
rmit-s3558425-mingqiang-zheng <s3558425@student.rmit.edu.au>
2017-08-24 22:18:26 +1000

    create login function

--- CineBooking/ViewController.swift
+++ CineBooking/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var userTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    let username = "admin"
+    let password = "password"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
     }
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func loginBtn(_ sender: AnyObject) {
+        if userTextField.text == username &&
+            passwordTextField.text == password{
+            print("login successful")
+        }
+        else{
+            print("try again!")
+        }
+    }
 
 }
 
